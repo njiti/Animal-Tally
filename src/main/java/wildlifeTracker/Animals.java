60a81431f24ba6abcca6ecc52b80af9afb6b4b07
njiti <kamaukan77@gmail.com>
2022-01-13 10:02:35 -0930

    Updated my Animals file.

--- src/main/java/wildlifeTracker/Animals.java
+++ src/main/java/wildlifeTracker/Animals.java
@@ -1,4 +1,42 @@
 package wildlifeTracker;
+import org.sql2o.*;
 
-public class Animals {
+
+
+public abstract class Animals {
+    public int id;
+    public String name;
+    public boolean endangered;
+    private Animals newAnimal;
+
+
+    public int getId() {
+        return id;
+    }
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public boolean equals(Object otherAnimal){
+        if(otherAnimal instanceof Animals){
+            return this.getName().equals(newAnimal.getName()) &&
+                    this.getId()==(newAnimal.getId());
+        } else {
+            return false;
+        }
+    }
+    public void save(){
+        if (name.equals("") ) {
+            throw new IllegalArgumentException("You must Enter Name.");
+        }
+        try(Connection connect = DB.sql2o.open()){
+            String sql = "INSERT INTO animals (name, endangered) VALUES (:name, :endangered);";
+            this.id = (int) connect.createQuery(sql, true)
+                    .addParameter("name", this.name)
+                    .addParameter("endangered", this.endangered)
+                    .executeUpdate()
+                    .getKey();
+        }
+    }
 }
