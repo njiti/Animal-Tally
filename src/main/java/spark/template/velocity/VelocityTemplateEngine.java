ad815eddae0877c5e0f07812bef4c1635a605523
njiti <kamaukan77@gmail.com>
2022-01-13 10:00:21 -0930

    Updated my VelocityTemplateEngine.java file.

--- src/main/java/spark/template/velocity/VelocityTemplateEngine.java
+++ src/main/java/spark/template/velocity/VelocityTemplateEngine.java
@@ -1,4 +1,77 @@
+/*
+ * Copyright 2013 Per Wendel
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package spark.template.velocity;
 
-public class VelocityTemplateEngine {
+import java.io.IOException;
+import java.io.StringWriter;
+import java.util.Map;
+import java.util.Properties;
+
+import org.apache.velocity.Template;
+import org.apache.velocity.VelocityContext;
+import org.apache.velocity.app.VelocityEngine;
+
+import spark.ModelAndView;
+import spark.TemplateEngine;
+
+/**
+ * Template Engine based on Apache Velocity.
+ */
+public class VelocityTemplateEngine extends TemplateEngine {
+
+    private final VelocityEngine velocityEngine;
+
+    /**
+     * Constructor
+     */
+    public VelocityTemplateEngine() throws Exception {
+        Properties properties = new Properties();
+        properties.setProperty("resource.loader", "class");
+        properties.setProperty(
+                "class.resource.loader.class",
+                "org.apache.velocity.runtime.resource.loader.ClasspathResourceLoader");
+        velocityEngine = new org.apache.velocity.app.VelocityEngine(properties);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public String render(ModelAndView modelAndView) {
+        Template template = null;
+        try {
+            template = velocityEngine.getTemplate(modelAndView.getViewName());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        Object model = modelAndView.getModel();
+        if (model instanceof Map) {
+            Map<?, ?> modelMap = (Map<?, ?>) model;
+            VelocityContext context = new VelocityContext(modelMap);
+            StringWriter writer = new StringWriter();
+            try {
+                template.merge(context, writer);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return writer.toString();
+        } else {
+            throw new IllegalArgumentException("modelAndView must be of type java.util.Map");
+        }
+    }
+
 }
